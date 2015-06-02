1dc23e64fe0f6e3d2f32b234b01e96faf255939e
Rodrigo Molina Munhoz <rodrigo_molina_munhoz@hotmail.com>
2015-06-02 17:41:23 -0300

    feat(TurboRango): Métodos e Main

--- src/TurboRango.Dominio/TurboRango.ImportadorXML/Program.cs
+++ src/TurboRango.Dominio/TurboRango.ImportadorXML/Program.cs
@@ -11,34 +11,43 @@ namespace TurboRango.ImportadorXML
     {
         static void Main(string[] args)
         {
-            Restaurante restaurante = new Restaurante();
-            //restaurante
-            Console.WriteLine(restaurante.Capacidade.HasValue ? 
-                              restaurante.Capacidade.Value.ToString() : "oi");
+            #region Exemplos
+                Restaurante restaurante = new Restaurante();
+                //restaurante
+                Console.WriteLine(restaurante.Capacidade.HasValue ? 
+                                  restaurante.Capacidade.Value.ToString() : "oi");
             
-            restaurante.Nome = String.Empty + " ";
+                restaurante.Nome = String.Empty + " ";
 
-            Console.WriteLine(restaurante.Nome ?? "nulo!!");
+                Console.WriteLine(restaurante.Nome ?? "nulo!!");
 
-            Console.WriteLine(!string.IsNullOrEmpty(restaurante.Nome.Trim()) ? "Tem valor" : "Não tem valor");
+                Console.WriteLine(!string.IsNullOrEmpty(restaurante.Nome.Trim()) ? "Tem valor" : "Não tem valor");
 
-            var oQueEuGosto = "bacon";
-            var texto = String.Format("Eu gosto de {0}", oQueEuGosto);
-            //var texto = String.Format("Eu gosto de \{oQueEuGosto}");
+                var oQueEuGosto = "bacon";
+                var texto = String.Format("Eu gosto de {0}", oQueEuGosto);
+                //var texto = String.Format("Eu gosto de \{oQueEuGosto}");
 
-            StringBuilder pedreiro = new StringBuilder();
-            pedreiro.Append("!!!!!!!!");
-            pedreiro.AppendFormat("Eu gosto de {0}", oQueEuGosto);
+                StringBuilder pedreiro = new StringBuilder();
+                pedreiro.Append("!!!!!!!!");
+                pedreiro.AppendFormat("Eu gosto de {0}", oQueEuGosto);
 
-            //object obj = "oi";
-            //int? a = obj as int?;
-            //int res = 12 + a.Value;
+                //object obj = "oi";
+                //int? a = obj as int?;
+                //int res = 12 + a.Value;
 
-            object obj = "1";
-            int a = Convert.ToInt32(obj);
-            int res = 12 + a;
+                object obj = "1";
+                int a = Convert.ToInt32(obj);
+                int res = 12 + a;
 
-            Console.WriteLine(pedreiro);
+                Console.WriteLine(pedreiro);
+            #endregion
+
+            const string nomeArquivo = "restaurantes.xml";
+
+            var restaurantesXML = new RestaurantesXML(nomeArquivo);
+            var nomes  = restaurantesXML.ObterNomes();
+            var capacidadeMedia = restaurantesXML.CapacidadeMedia();
+            var capacidadeMaxima = restaurantesXML.CapacidadeMax();
         }
     }
 }
---
+++ src/TurboRango.Dominio/TurboRango.ImportadorXML/RestaurantesXML.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace TurboRango.ImportadorXML
+{
+    public class RestaurantesXML
+    {
+        public string NomeArquivo {get; private set;}
+
+        
+        /// <summary>
+        /// Constrói restaurantes a partir de um numero de arquivo.
+        /// </summary>
+        /// <param name="nomeArquivo"></param>
+        public RestaurantesXML(string nomeArquivo)
+        {
+            this.NomeArquivo = NomeArquivo;
+        }
+
+        public IList<string> ObterNomes()
+        {
+            //var resultado = new List<string>();
+
+            //var nodos = XDocument.Load(NomeArquivo).Descendants("restaurante");
+
+            //foreach (var item in nodos)
+            //{
+            //    resultado.Add(item.Attribute("nome").Value);
+            //}
+
+            //return resultado;
+
+            /*
+            return XDocument
+                            .Load(NomeArquivo)
+                            .Descendants("restaurante")
+                            .Select(n => n.Attribute("nome").Value)
+                            .ToList();
+            */
+            
+            return (
+                from n in XDocument.Load(NomeArquivo).Descendants("restaurante")
+                orderby n.Attribute("nome").Value
+                select n.Attribute("nome").Value
+                ).ToList();
+             
+        }
+
+        public double CapacidadeMedia()
+        {
+            return (
+                from n in XDocument.Load(NomeArquivo).Descendants("restaurante")
+                select Convert.ToInt32(n.Attribute("capacidade").Value)
+                ).Average();
+        }
+
+        public double CapacidadeMax()
+        {
+            var mad = (
+                from n in XDocument.Load(NomeArquivo).Descendants("restaurante")
+                select Convert.ToInt32(n.Attribute("capacidade").Value)
+                );
+            return mad.Max();
+        }
+    }
+}
