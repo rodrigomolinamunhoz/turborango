6c891a83bcec4002aaf966ada464b07c7d9bf407
Bernardo Bosak de Rezende <bernardobrezende@gmail.com>
2015-06-04 01:41:29 -0300

    chore(web.config.release): atualizar connectionString com banco do cloud.

--- src/TurboRango/TurboRango.Web/Models/IdentityModels.cs
+++ src/TurboRango/TurboRango.Web/Models/IdentityModels.cs
@@ -21,7 +21,7 @@ namespace TurboRango.Web.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("TurboRangoConnStr", throwIfV1Schema: false)
         {
         }
 
