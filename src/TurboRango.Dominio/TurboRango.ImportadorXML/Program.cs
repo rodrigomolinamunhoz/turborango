72166e4fb2f5dbf383c4f7138db3daf851818070
Rodrigo Molina Munhoz <rodrigo_molina_munhoz@hotmail.com>
2015-06-07 17:23:53 -0300

    feat(TurboRango): Exercício 1: 1B

--- src/TurboRango.Dominio/TurboRango.ImportadorXML/Program.cs
+++ src/TurboRango.Dominio/TurboRango.ImportadorXML/Program.cs
@@ -72,6 +72,25 @@ namespace TurboRango.ImportadorXML
            
                 var restaurantes = new Restaurantes(stringDeConexao);
 
+                restaurantes.Inserir(new Restaurante
+                {
+                    Nome = "Tiririca",
+                    Capacidade = 50,
+                    Categoria = Categoria.FastFood,
+                    Contato = new Contato
+                    {
+                        Site = "http://github.com/tiririca",
+                        Telefone = "5555 5555"
+                    },
+                    Localizacao = new Localizacao
+                    {
+                        Bairro = "Vila Nova",
+                        Logradouro = "ERS 239, 2755",
+                        Latitude = -29.6646122,
+                        Longitude = -51.1188255
+                    }
+                });
+
             #endregion
 
         }
--- src/TurboRango.Dominio/TurboRango.ImportadorXML/Restaurantes.cs
+++ src/TurboRango.Dominio/TurboRango.ImportadorXML/Restaurantes.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TurboRango.Dominio;
 
 namespace TurboRango.ImportadorXML
 {
@@ -14,6 +16,72 @@ namespace TurboRango.ImportadorXML
 
         public Restaurantes(string connectionString) {
             this.connectionString = connectionString;
+        }
+
+        internal void Inserir(Restaurante restaurantes)
+        {
+            int idRestaurante;
+            using (var connection = new SqlConnection(this.connectionString))
+            {
+                using (var inserirRestaurante = new SqlCommand("INSERT INTO [dbo].[Restaurante]"
+                    + "([Capacidade],[Nome],[Categoria],[ContatoId],[LocalizacaoId])"
+                    + "VALUES (@Capacidade, @Nome, @Categoria, @ContatoId, @LocalizacaoId)", connection))
+                {
+                    inserirRestaurante.Parameters.Add("@Capacidade", SqlDbType.Int).Value = restaurantes.Capacidade;
+                    inserirRestaurante.Parameters.Add("@Nome", SqlDbType.NVarChar).Value = restaurantes.Nome;
+                    inserirRestaurante.Parameters.Add("@Categoria", SqlDbType.Int).Value = restaurantes.Categoria;
+                    inserirRestaurante.Parameters.Add("@ContatoId", SqlDbType.Int).Value = this.InserirContato(restaurantes.Contato);
+                    inserirRestaurante.Parameters.Add("@LocalizacaoId", SqlDbType.Int).Value = this.InserirLocalizacao(restaurantes.Localizacao);
+
+                    connection.Open();
+                    idRestaurante = Convert.ToInt32(inserirRestaurante.ExecuteScalar());
+
+                    Debug.WriteLine("Restaurante criado! ID no banco: {0}", idRestaurante);
+                }
+            }
+        }
+
+        internal int InserirContato(Contato contato)
+        {
+            int idContato;
+            using (var connection = new SqlConnection(this.connectionString))
+            {
+                using (var inserirContato = new SqlCommand("INSERT INTO [dbo].[Contato] ([Site],[Telefone])"
+                     + "VALUES (@Site, @Telefone); SELECT @@IDENTITY", connection))
+                {
+                    inserirContato.Parameters.Add("@Site", SqlDbType.NVarChar).Value = contato.Site;
+                    inserirContato.Parameters.Add("@Telefone", SqlDbType.NVarChar).Value = contato.Telefone;
+
+                    connection.Open();
+                    idContato = Convert.ToInt32(inserirContato.ExecuteScalar());
+
+                    Debug.WriteLine("Contato criado! ID no banco: {0}", idContato);
+                }
+                return idContato;
+            }
+        }
+
+        internal int InserirLocalizacao(Localizacao localizacao)
+        {
+            int idLocalizacao;
+            using (var connection = new SqlConnection(this.connectionString))
+            {
+                using (var inserirLocalizacao = new SqlCommand("INSERT INTO [dbo].[Localizacao]"
+                     + "([Bairro],[Logradouro],[Latitude],[Longitude])"
+                     + "VALUES (@Bairro, @Logradouro, @Latitude, @Longitude); SELECT @@IDENTITY", connection))
+                {
+                    inserirLocalizacao.Parameters.Add("@Bairro", SqlDbType.NVarChar).Value = localizacao.Bairro;
+                    inserirLocalizacao.Parameters.Add("@Logradouro", SqlDbType.NVarChar).Value = localizacao.Logradouro;
+                    inserirLocalizacao.Parameters.Add("@Latitude", SqlDbType.Float).Value = localizacao.Latitude;
+                    inserirLocalizacao.Parameters.Add("@Longitude", SqlDbType.Float).Value = localizacao.Longitude;
+
+                    connection.Open();
+                    idLocalizacao = Convert.ToInt32(inserirLocalizacao.ExecuteScalar());
+
+                    Debug.WriteLine("Localizacao criada! ID no banco: {0}", idLocalizacao);
+                }
+                return idLocalizacao;
+            }           
         }
     }
 }
