dd5ab6bbd769425d4f11312f67d29db28acb9e8b
Bernardo Bosak de Rezende <bernardobrezende@gmail.com>
2015-06-08 16:16:04 -0300

    feat(Initial): versionar solução ASP.NET MVC.

---
+++ src/TurboRango/TurboRango.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TurboRango.Web.Startup))]
+namespace TurboRango.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
