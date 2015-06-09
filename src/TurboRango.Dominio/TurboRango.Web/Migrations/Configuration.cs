b60e447e0d06e23e5aaf15d868f8655079f7096d
Rodrigo Molina Munhoz <rodrigo_molina_munhoz@hotmail.com>
2015-06-09 14:53:43 -0300

    feat(TurboRango): adicionar todos restaurantes do XML no seed

--- src/TurboRango.Dominio/TurboRango.Web/Migrations/Configuration.cs
+++ src/TurboRango.Dominio/TurboRango.Web/Migrations/Configuration.cs
@@ -4,6 +4,7 @@ namespace TurboRango.Web.Migrations
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
+    using TurboRango.Dominio;
 
     internal sealed class Configuration : DbMigrationsConfiguration<TurboRango.Web.Models.ApplicationDbContext>
     {
@@ -27,6 +28,132 @@ namespace TurboRango.Web.Migrations
             //      new Person { FullName = "Rowan Miller" }
             //    );
             //
+            context.Restaurantes.AddOrUpdate(
+                  p => p.Nome, new Restaurante
+            {
+                Nome = "Tiririca",
+                Capacidade = 50,
+                Categoria = Categoria.FastFood,
+                Contato = new Contato
+                {
+                    Site = "http://github.com/tiririca",
+                    Telefone = "5555 5555"
+                },
+                Localizacao = new Localizacao
+                {
+                    Bairro = "Vila Nova",
+                    Logradouro = "ERS 239, 2755",
+                    Latitude = -29.6646122,
+                    Longitude = -51.1188255
+                }
+            },
+            new Restaurante
+            {
+                Nome = "GARF�O RESTAURANTE E PIZZARIA",
+                Capacidade = 100,
+                Categoria = Categoria.Comum,
+                Contato = new Contato
+                {
+                    Site = "www.garfao.com",
+                    Telefone = "(51) 3587-7700"
+                },
+                Localizacao = new Localizacao
+                {
+                    Bairro = "Liberdade",
+                    Logradouro = "Rua Sete de Setembro, 1045 - Liberdade",
+                    Latitude = -29.712571,
+                    Longitude = -51.13636
+                }
+            },
+            new Restaurante
+            {
+                Nome = "CHURRASCARIA PRIMAVERA",
+                Capacidade = 110,
+                Categoria = Categoria.Churrascaria,
+                Contato = new Contato
+                {
+                    Site = "www.grupoprimaveranh.com.br",
+                    Telefone = "3595-8081"
+                },
+                Localizacao = new Localizacao
+                {
+                    Bairro = "Primavera",
+                    Logradouro = "BR 116, 2554, Km 31",
+                    Latitude = -29.693741,
+                    Longitude = -51.144554
+                }
+            },
+            new Restaurante
+            {
+                Nome = "RESTAURANTE HABITU�",
+                Capacidade = 90,
+                Categoria = Categoria.Comum,
+                Contato = new Contato
+                {
+                    Site = "www.habitue.com.br",
+                    Telefone = "35931602"
+                },
+                Localizacao = new Localizacao
+                {
+                    Bairro = "Centro",
+                    Logradouro = "Joaquim Nabuco, 1117",
+                    Latitude = -29.6829086,
+                    Longitude = -51.1263546
+                }
+            },
+            new Restaurante
+            {
+                Nome = "PICA-PAU LANCHES",
+                Capacidade = 30,
+                Categoria = Categoria.FastFood,
+                Contato = new Contato
+                {
+                    Site = "www.picapaulanches.com",
+                    Telefone = "(51) 3593-8079"
+                },
+                Localizacao = new Localizacao
+                {
+                    Bairro = "Rio Branco",
+                    Logradouro = "Rua: Jos� do Patroc�nio, 880",
+                    Latitude = -29.68339,
+                    Longitude = -51.135376
+                }
+            },
+            new Restaurante
+            {
+                Nome = "ADAMS PIZZARIA",
+                Capacidade = 130,
+                Categoria = Categoria.Pizzaria,
+                Contato = new Contato
+                {
+                    Site = "www.admspizzaria.com.br",
+                    Telefone = "35823000"
+                },
+                Localizacao = new Localizacao
+                {
+                    Bairro = "Industrial",
+                    Logradouro = "Avenida Pedro Adams Filho, 4466",
+                    Latitude = -29.6933741,
+                    Longitude = -51.1283531
+                }
+            }, new Restaurante
+                {
+                    Nome = "OL� ARMAZ�M MEXICANO",
+                    Capacidade = 30,
+                    Categoria = Categoria.CozinhaMexicana,
+                    Contato = new Contato
+                {
+                    Site = "",
+                    Telefone = "3279-8828"
+                },
+                    Localizacao = new Localizacao
+                    {
+                        Bairro = "Centro",
+                        Logradouro = "Rua Gomes Portinho, 448",
+                        Latitude = -29.682078,
+                        Longitude = -51.125199
+                    }
+                });
         }
     }
 }
