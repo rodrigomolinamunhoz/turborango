1953c2f6a41d88bf65cb7e5720db6f8b704330cf
Rodrigo Molina Munhoz <rodrigo_molina_munhoz@hotmail.com>
2015-06-10 17:06:16 -0300

    feat(TurboRango): Criação do Controller e Index Sorteios

---
+++ src/TurboRango.Dominio/TurboRango.Web/Controllers/SorteiosController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TurboRango.Web.Models;
+
+namespace TurboRango.Web.Controllers
+{
+    public class SorteiosController : Controller
+    {
+        ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Sorteios
+        public ActionResult Index()
+        {
+            ViewBag.QtdRestaurantes = db.Restaurantes.Count();
+
+            return View();
+        }
+    }
+}
