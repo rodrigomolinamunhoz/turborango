aba8a4fe026217b52c71c8c6a94cf0193f2604d5
Bernardo Bosak de Rezende <bernardobrezende@gmail.com>
2015-06-10 16:02:31 -0300

    feat(haversine): versionar script e criar bundle para o mesmo.

--- src/TurboRango/TurboRango.Web/App_Start/BundleConfig.cs
+++ src/TurboRango/TurboRango.Web/App_Start/BundleConfig.cs
@@ -11,6 +11,9 @@ namespace TurboRango.Web
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/haversine").Include(
+                        "~/Scripts/haversine.js"));
+
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
