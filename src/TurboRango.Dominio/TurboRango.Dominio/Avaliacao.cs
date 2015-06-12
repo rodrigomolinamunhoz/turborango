b3ff41c8ffe553faba869d75bc20b6abb0ee1b28
Rodrigo Molina Munhoz <rodrigo_molina_munhoz@hotmail.com>
2015-06-11 22:15:24 -0300

    feat(TurboRango): Criação Classe Avaliacao

---
+++ src/TurboRango.Dominio/TurboRango.Dominio/Avaliacao.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TurboRango.Dominio
+{
+    public class Avaliacao : Entidade
+    {
+        public double nota { get; set; }
+        public double media { get; set; }
+        public DateTime Data { get; set; }
+        public virtual Restaurante Restaurante { get; set; }
+    }
+}
